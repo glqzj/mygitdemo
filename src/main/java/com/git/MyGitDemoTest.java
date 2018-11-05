6cd93e75853742e0e2b8139b15d9e8e919b2de98
dsf <1378981437@qq.com>
2018-11-05 16:28:35 +0800

    我重新跟新了项目

--- src/main/java/com/git/MyGitDemoTest.java
+++ src/main/java/com/git/MyGitDemoTest.java
@@ -3,5 +3,6 @@ package com.git;
 public class MyGitDemoTest {
     public static void main(String[] args) {
         System.out.println("奉二狗傻逼！@！！！！！！");
+        System.out.println("我又添加了一个东东！！！你试一下吧！！");
     }
 }
